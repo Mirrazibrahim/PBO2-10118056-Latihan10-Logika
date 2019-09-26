86226d4a8a903cc71224fe2202186e71550f2445
Mirrazibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-09-26 13:06:55 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118056/latihan10/logika/PBOIF210118056Latihan10Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118056.latihan10.logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Mirrazibrahim
+ * KELAS    : IF-2
+ * NIM      : 10118056
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * Status dewasa
+ */
+public class PBOIF210118056Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            int umur = 19;
+            boolean dewasa;
+            dewasa = (umur >=17);
+            System.out.println("Status Dewasa : "+ dewasa);
+        }
+    }
+    
+
